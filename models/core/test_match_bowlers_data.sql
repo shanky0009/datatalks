f2b3ac3e8d61690406736b658d9b87c20ac17bc5
smahale8451@gmail.com <smahale8451@gmail.com>
2022-04-04 16:52:08 +0000

    added model for wickets

--- models/core/test_match_bowlers_data.sql
+++ models/core/test_match_bowlers_data.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='table') }}
 
-select EXTRACT(YEAR from match_date) as year, 
-    batter, 
-    max(match_date) as md,
-    sum(CAST(no_of_runs_from_bat as int64)) as no_of_runs_in_year
-from {{ ref('runs_data') }}
-where match_type = 'Test'
-group by year, batter
+select bowler, 
+    wicket_kind, 
+    count(1) as c
+from {{ ref('wickets_data') }}
+where wicket_kind in ("\"caught\"", "\"bowled\"", "\"lbw\"", "\"hit wicket\"", "\"stumped\"", "\"caught and bowled\"")
+and match_type = 'Test'
+group by bowler, wicket_kind
